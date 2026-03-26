1ca8f347f62190d470479828423fe57c59ed1ba4
vikram soni <jays8174@gmail.com>
2026-03-27 02:11:20 +0530

    react setup create router install dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Service from "./pages/Service";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
